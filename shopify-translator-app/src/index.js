00e5e84a04d037c328db00b80113fb322d3595b8
David Elliott <dave.elliott@gmail.com>
2020-09-14 08:53:02 -0400

    cleanup

--- shopify-translator-app/src/index.js
+++ shopify-translator-app/src/index.js
@@ -4,56 +4,31 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { PlayerContextProvider } from "./PlayerContext";
-import { PlayerBoundariesContextProvider } from "./PlayerBoundariesContext";
-import { HighlighterContextProvider } from "./HighlighterContext";
 
 import { SpeechSynthContextProvider } from "./SpeechSynthContext";
 
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
-import { isMobile } from "react-device-detect";
 
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-if (isMobile) {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <SpeechSynthContextProvider>
-          <HighlighterContextProvider>
-            <PlayerBoundariesContextProvider>
-              <PlayerContextProvider>
-                <App />
-              </PlayerContextProvider>
-            </PlayerBoundariesContextProvider>
-          </HighlighterContextProvider>
-        </SpeechSynthContextProvider>
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <SpeechSynthContextProvider>
-          <HighlighterContextProvider>
-            <PlayerBoundariesContextProvider>
-              <PlayerContextProvider>
-                <App />
-              </PlayerContextProvider>
-            </PlayerBoundariesContextProvider>
-          </HighlighterContextProvider>
-        </SpeechSynthContextProvider>
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <SpeechSynthContextProvider>
+        <PlayerContextProvider>
+          <App />
+        </PlayerContextProvider>
+      </SpeechSynthContextProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
