7ca5df3e23a254bb48adbd130bdb1e25f335cf26
David Elliott <dave.elliott@gmail.com>
2020-09-18 07:10:32 -0400

    title change

--- shopify-translator-app/src/index.js
+++ shopify-translator-app/src/index.js
@@ -16,6 +16,8 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+document.title = "Shopify Masters - Translated!";
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
