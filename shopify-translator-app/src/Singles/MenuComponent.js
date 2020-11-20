32be4782738fc83d1eeb9291a778a81c13530831
David Elliott <dave.elliott@gmail.com>
2020-11-20 07:13:33 -0500

    test

--- shopify-translator-app/src/Singles/MenuComponent.js
+++ shopify-translator-app/src/Singles/MenuComponent.js
@@ -15,20 +15,20 @@ let divStyle = {
 };
 
 function MenuComponent() {
-  // const [profileName, setProfileName] = React.useState("none");
+  const [profileName, setProfileName] = React.useState("none");
 
-  // function onSignIn(googleUser) {
-  //   var profile = googleUser.getBasicProfile();
-  //   // var id_token = googleUser.getAuthResponse().id_token;
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    // var id_token = googleUser.getAuthResponse().id_token;
 
-  //   console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  //   console.log("Name: " + profile.getName());
-  //   console.log("Image URL: " + profile.getImageUrl());
-  //   console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
-  //   // console.log("Email: " + id_token); // This is null if the 'email' scope is not present.
+    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+    // console.log("Email: " + id_token); // This is null if the 'email' scope is not present.
 
-  //   setProfileName(profile.getName());
-  // }
+    setProfileName(profile.getName());
+  }
 
   return (
     <Wrapper>
